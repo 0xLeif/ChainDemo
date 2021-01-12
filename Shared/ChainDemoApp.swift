0a157ac1f5b70541917c03365075e2f60a27a449
0xLeif <zmeriksen@icloud.com>
2021-01-12 17:25:53 -0600

    Initial Commit

---
+++ Shared/ChainDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChainDemoApp.swift
+//  Shared
+//
+//  Created by Zach Eriksen on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ChainDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
