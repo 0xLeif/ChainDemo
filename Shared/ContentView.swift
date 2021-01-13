2a9f260abb4e9b89fda4f236ebf5fac33a4aae38
0xLeif <zmeriksen@icloud.com>
2021-01-12 18:25:06 -0600

    init

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,54 @@
 //
 
 import SwiftUI
+import Chain
+import SURL
 
 struct ContentView: View {
+    @State private var text = "Hello, world!"
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(text)
             .padding()
+            .onAppear {
+                Chain
+                    .link(reportScreenView,
+                          .link(setLoadingState,
+                                .background(fetchData,
+                                            .end)
+                          )
+                    )
+                    .run()
+            }
+    }
+    
+    func reportScreenView() {
+        reportAnalytic(screenNamed: "Home")
+    }
+    
+    func setLoadingState() {
+        update(text: "Loading...")
+    }
+    
+    func fetchData() {
+        "https://raw.githubusercontent.com/0xLeif/0xLeif/master/README.md"
+            .url?
+            .get  { (data, response, error) in
+                
+                Chain.link({
+                    update(text: String(data: data!, encoding: .utf8) ?? "😬")
+                },
+                .complete {
+                    reportAnalytic(event: "home_data_loaded")
+                })
+                .run()
+                
+                
+            }
+    }
+    
+    func update(text value: String) {
+        text = value
     }
 }
 
@@ -19,3 +62,8 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+/// MARK: Mock Functions
+
+func reportAnalytic(event: String) { }
+func reportAnalytic(screenNamed: String) { }
